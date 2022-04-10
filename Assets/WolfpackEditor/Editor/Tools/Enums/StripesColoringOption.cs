9769ec51d6fd1adee331ea333d081e3971b3c479
TheAshenWolf <jirirkas.skola@gmail.com>
2022-04-10 20:51:18 +0200

    Added few enums to further expand the modules

---
+++ Assets/WolfpackEditor/Editor/Tools/Enums/StripesColoringOption.cs
@@ -0,0 +1,10 @@
+﻿namespace WolfpackEditor.Editor.Tools.Enums
+{
+  public enum StripesColoringOption
+  {
+    LightDark = 0,
+    CustomColors = 1,
+    ByGroup = 2
+    
+  }
+}
---
+++ Assets/WolfpackEditor/Editor/Tools/Enums/StripesOption.cs
@@ -0,0 +1,10 @@
+﻿namespace WolfpackEditor.Editor.Tools.Enums
+{
+  public enum StripesOption
+  {
+    EvenOdd = 0,
+    ByGroup = 1, // Forces ByGroup in StripesColoringOption too
+    
+    
+  }
+}
